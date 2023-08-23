baf754a2e241bf6c7d63661723457b314ca390e4
Sohel <patvekarsuhel@gmail.com>
2023-08-23 17:05:01 +0530

    Static & Non-static method commit

---
+++ Static vs non-static/src/Test.java
@@ -0,0 +1,38 @@
+
+public class Test {
+	
+//	static method
+	/*public static int add(int a, int b) {
+	        return a + b;
+	    }*/
+	
+//	Non-static method
+	/*private double radius;
+
+    public Test(double radius) {
+        this.radius = radius;
+    }
+
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }*/
+
+	
+	public static void main(String[] args) {
+//	static method calling
+	/* int result = Test.add(5, 7);
+	   System.out.println("Result: " + result);*/
+		
+//	Non-static method calling
+		/*Test test = new Test(5);
+        double area = test.calculateArea();
+        System.out.println("Area of the circle: " + area);*/
+		
+		
+		
+		
+	    }
+}
+	
+
+
